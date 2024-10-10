ad5519056caa1ecf28210f8765e50a74660ea361
James Hawkins <jamesdonhawkins@gmail.com>
2024-10-10 10:15:28 -0700

    added prompts for embedding enrichments.

---
+++ prompts/chat_prompts.py
@@ -0,0 +1,28 @@
+DEFAULT_QUESTION_GEN_TMPL = """\
+Here is the context:
+{context_str}
+
+Given the contextual information, \
+generate {num_questions} questions this context can provide \
+specific answers to which are unlikely to be found elsewhere.
+
+Higher-level summaries of surrounding context may be provided \
+as well. Try using these summaries to generate better questions \
+that this context can answer.
+
+"""
+
+# text qa prompt
+TEXT_QA_SYSTEM_PROMPT = ChatMessage(
+    content=(
+        "You are an expert Q&A system that is trusted around the world.\n"
+        "Always answer the query using the provided context information, "
+        "and not prior knowledge.\n"
+        "Some rules to follow:\n"
+        "1. Never directly reference the given context in your answer.\n"
+        "2. Avoid statements like 'Based on the context, ...' or "
+        "'The context information ...' or anything along "
+        "those lines."
+    ),
+    role=MessageRole.SYSTEM,
+)
